a34dada5906bb7b1f45ba697a82675069e1d263b
hyxos <dw@1hx.ca>
2015-12-03 17:22:27 +0000

    built out book model

--- app/models/book.js
+++ app/models/book.js
@@ -2,7 +2,20 @@ var mongoose  = require('mongoose');
 var Schema    = mongoose.Schema;
 
 var BookSchema = new Schema({
-  name: String
+  title: String,
+  author: String,
+  pages: Number,
+  copyright: Number,
+  isbn: Number,
+  record_id: Number,
+  copies_and_availability: {
+    branch: String,
+    format: String,
+    location: String,
+    status: String,
+    call_number: String
+  }
+
 })
 
 module.exports = mongoose.model('Book', BookSchema);
