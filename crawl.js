d08948c1008bb297e2b17e96946625c7d47fd483
Connor Brathwaite <connorbrathwaite@Connors-MacBook-Pro.local>
2015-12-03 14:07:47 -0500

    crawler able to fetch hrefs from a search results page

--- crawl.js
+++ crawl.js
@@ -1,18 +1,20 @@
-var Crawler = require("simplecrawler");
-var myCrawler = new Crawler("www.torontopubliclibrary.ca/");
-myCrawler.maxDepth = 2;
+var cheerio = require('cheerio');
+var rp = require('request-promise');
 
-myCrawler.start();
+var options = {
+  uri: 'http://www.torontopubliclibrary.ca/search.jsp?N=37906+38758&Ntk=Keyword_Anywhere&advancedSearch=true',
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
 
-myCrawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
-  console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
-  console.log("It was a resource of type %s", response.headers['content-type']);
-
-    // Do something with the data in responseBuffer
-  //   crawler.discoverResources = function(buf, queueItem) {
-  //   // scan buffer for URLs, and then:
-
-  //   crawler.queueURL(aDiscoveredURL, queueItem);
-  // };
-});
-
+rp(options)
+  .then(function ($) {
+    var urls = $('div > div > div.title.align-top > a');
+    $(urls).each(function () {
+      console.log($(this).attr('href'));
+    })
+  })
+  .catch(function (err) {
+    console.log(err)
+  });
