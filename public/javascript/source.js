5e18f4597bf03f08c5b5aee7b600ef1821784dcc
Dino <glucose@gene.local>
2015-12-08 16:00:20 -0500

    basic react components

--- public/javascript/source.js
+++ public/javascript/source.js
@@ -1,13 +1,77 @@
 'use strict';
 
-class SearchComponent extends React.Component {
+class BookItem extends React.Component {
   render() {
+    var title = this.props.title
+    var author = this.props.author
     return (
-      <h1>Dude, what up?</h1>
-      );
+      <div className="card-panel white-text indigo">{title} by {author}</div>
+    )
   }
 }
 
+class BookList extends React.Component {
+  render() {
+    var items = [];
+    this.props.books.forEach(function(book) {
+      items.push(<BookItem title={book.title} author={book.author} />)
+     });
+    return (
+      <div>{items}</div>
+    );
+  }
+}
+
+class SearchBox extends React.Component {
+  handleChange() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.value
+      )
+  }
+  render() {
+    return (
+      <form>
+        <label htmlFor="search"></label>
+        <input className="input-block" type="text" id="search" placeholder="search books" value={this.props.filterText} />
+      </form>
+    );
+  }
+}
+
+class FilterableBookList extends React.Component {
+  getInitialState() {
+    return {
+      filterText: ''
+    }
+  }
+
+  handleUserInput(filterText) {
+    this.setState({
+      filterText: filterText
+    })
+  }
+  debugger;
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <SearchBox filterText={this.props.filterText}/>
+            <BookList books={this.props.books} filterText={this.props.filterText} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+var BOOKS = [
+{title: 'Harry Potter and the Chamber of Secrets', author: 'J.K. Rowling'},
+{title: 'The Lord of the Rings: Fellowship of the Ring', author: 'J.R.R. Tolkien'},
+{title: 'Dune', author: 'Frank Herbert'}
+]
+
 const renderTarget = document.createElement('div');
 document.body.appendChild(renderTarget);
-React.render(<SearchComponent />, renderTarget);
+ReactDOM.render(<FilterableBookList books={BOOKS} />, renderTarget);
+
