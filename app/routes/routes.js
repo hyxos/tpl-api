cb98e8945fb056ccfddae90d1fb485806f4a6b11
Connor Brathwaite <connorbrathwaite@Connors-MacBook-Pro.local>
2015-12-09 14:54:22 -0500

    removing excess vars

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,39 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var Book = require '../../db/config'
+var express = require('express'),
+  router = express.Router(),
+  Book = require ('../../db/config')
 
-router.get('/', (req, res) => res.send("Hello World"))
+router.get('/', (req, res) => res.send('in api'))
 
 router.get('/books', (req,res) => {
-  .post((req, res) => {
-    var book = new Book()
-    book.title = req.body.title
-    book.save(() => res.json({ message: 'Book created!' }))
-  })
-
   .get((req, res) => {
     var query = {}
     // TODO: QUERYING e.g: books?branch=North+York+Central+Library
     if (req.query.branch) query.branch = req.query.branch
     if (req.query.title) query.title = req.query.title
     Book.find(query).exec((err, books) => res.json(books))
-  })
-  // on routes that end in /books/:book_id
-  router.route('/books/:book_id')
-  .get((req, res) => {
-    Book.findById(req.params.Book_id, (err, Book) => {
-      if (err) res.send(err)
-      res.json(Book)
-    })
-  })
-  .put((req, res) => {
-    Book.findById(req.params.Book_id, (err, Book) => {
-      Book.name = req.body.name
-      Book.save(() => res.json({ message: 'Book updated!' }))
-    })
-  })
-  .delete((req, res) => {
-    Book.remove({ _id: req.params.book_id}, (err, book) => res.json({ message: 'Successfully deleted' })
   })
 }
 
