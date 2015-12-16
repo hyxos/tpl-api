e939bffcb156ff2ee680d007d34f07e271cc036f
Connor Brathwaite <connor.brathwaite@gmail.com>
2015-12-16 10:53:44 -0500

    styling

--- app/server.js
+++ app/server.js
@@ -11,10 +11,10 @@ app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json())
 app.listen(3000, () => console.log('magic happens on port 3000'))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
 });
 
 app.get('/books', (req, res) => {
